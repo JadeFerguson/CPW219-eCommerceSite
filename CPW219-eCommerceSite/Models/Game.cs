d7268f0c23af8990699c30123803a8688a88d822
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-05 09:12:55 -0700

    Added Game class

---
+++ CPW219-eCommerceSite/Models/Game.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CPW219_eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single game available for purchase
+    /// </summary>
+    public class Game
+    {
+        /// <summary>
+        /// The unique identifier for each game product
+        /// </summary>
+        [Key]
+        public int GameId { get; set; }
+
+        /// <summary>
+        /// The offical title of the video game
+        /// </summary>
+        [Required]
+        public string Title { get; set; }
+
+        /// <summary>
+        /// The sales price of the game
+        /// </summary>
+        // Since a double a double has to have a value
+        [Range(0, 1000)]
+        public double Price { get; set; }
+
+        //TODO: Add Rating
+    }
+}
