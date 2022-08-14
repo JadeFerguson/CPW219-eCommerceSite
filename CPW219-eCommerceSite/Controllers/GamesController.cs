84e3fbe5bfcdd85228eec28545ef4771427ed786
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-14 13:47:04 -0700

    Created create game view

---
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class GamesController : Controller
+    {
+        [HttpGet]
+        // To add razor view right click on create then will show up
+        public IActionResult Create()
+        {
+            
+            return View();
+        }
+    }
+}
