12021d71070cc594e730181f17e0743532defa20
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-17 21:57:35 -0700

    Added HttpGet for edit

--- CPW219-eCommerceSite/Controllers/GamesController.cs
+++ CPW219-eCommerceSite/Controllers/GamesController.cs
@@ -59,5 +59,18 @@ namespace CPW219_eCommerceSite.Controllers
             }
             return View(game);
         }
+
+        //async needs task 
+        public async Task<IActionResult> Edit(int id)
+        {
+            Game gameToEdit = await _context.Games.FindAsync(id);
+
+            if (gameToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(gameToEdit);
+        }
     }
 }
