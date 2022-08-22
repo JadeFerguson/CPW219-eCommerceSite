22496fcc1f92881446f34b3bc6c52598edc6de4d
Jade Ferguson <jadesnider2440@gmail.com>
2022-08-22 09:21:45 -0700

    Added register view

---
+++ CPW219-eCommerceSite/Controllers/MembersController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CPW219_eCommerceSite.Controllers
+{
+    public class MembersController : Controller
+    {
+        // added this in controllers and is a Mvc empty
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
